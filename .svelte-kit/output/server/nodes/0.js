d59b1b9da44d50765e3d4cd5611a353f05639c4b
Jaibey Pisco <jaibey.pisco@tecsup.edu.pe>
2022-06-28 00:39:41 -0500

    initial gh-pages commit

---
+++ .svelte-kit/output/server/nodes/0.js
@@ -0,0 +1,7 @@
+import * as module from '../entries/fallbacks/layout.svelte.js';
+
+export { module };
+export const index = 0;
+export const entry = 'layout.svelte-d8401fb8.js';
+export const js = ["layout.svelte-d8401fb8.js","chunks/index-c3650d4a.js"];
+export const css = [];
---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,7 @@
+import * as module from '../entries/fallbacks/error.svelte.js';
+
+export { module };
+export const index = 1;
+export const entry = 'error.svelte-2016a9a5.js';
+export const js = ["error.svelte-2016a9a5.js","chunks/index-c3650d4a.js"];
+export const css = [];
